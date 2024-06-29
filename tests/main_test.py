c95f327de571a3be524266dd99eea43cd59f699a
Senthil Kumaran <senthil@python.org>
2024-06-28 18:04:39 -0700

    Added first test.

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,5 +1,9 @@
-from main import add_numbers
+from main import get_top_urls
 
 
-def test_add_numbers():
-    assert add_numbers(2, 2) == 4
+def test_get_top_urls():
+    scores_dict = {"url1": 10, "url2": 5, "url3": 8, "url4": 3, "url5": 6}
+    n = 3
+
+    expected_result = {"url1": 10, "url3": 8, "url5": 6}
+    assert get_top_urls(scores_dict, n) == expected_result
