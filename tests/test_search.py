a49c1153b15d5239ef3fda4fbc21c761a38eb8e8
Senthil Kumaran <senthil@python.org>
2024-06-30 16:00:56 -0700

    Updated test scores.

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,6 +1,6 @@
 import unittest
 
-from search import normalize_string
+from search import normalize_string, update_url_scores
 
 
 class TestNormalizeString(unittest.TestCase):
@@ -14,3 +14,19 @@ class TestNormalizeString(unittest.TestCase):
         for input_string, expected_output in input_expected_pairs:
             with self.subTest(input_string=input_string):
                 self.assertEqual(normalize_string(input_string), expected_output)
+
+    def test_update_url_scores(self):
+        old_scores = {
+            "example.com": 10,
+            "google.com": 5,
+        }
+        new_scores = {
+            "example.com": 5,
+            "github.com": 3,
+        }
+        expected_scores = {
+            "example.com": 15,
+            "google.com": 5,
+            "github.com": 3,
+        }
+        self.assertEqual(update_url_scores(old_scores, new_scores), expected_scores)
